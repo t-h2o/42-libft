2e166da1747b4865a18a3ed460ea085c603c70fd
theo-grivel <togriv@gmail.com>
2021-10-30 19:48:58 +0200

    add in libft.h simple function

--- libft.h
+++ libft.h
@@ -3,35 +3,43 @@
 #define LIBFT_H
 #endif
 
-	// string to int
+	// whithout extern functions
 
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char* s);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_bzero(void *b, size_t len);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+char	*ft_strlcpy(char *dest, const char *src, size_t n);
+char	*ft_strlcat(char *dest, const char *src, size_t n);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 int	ft_atoi(const char* str);
 
 
-	// string to size_t
+	// only malloc function
 
-size_t	ft_strlen(const char* s);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 
+	
+	// Other
 
-	// int to int
-
-int	ft_isalpha(int c);
-int	ft_isalnum(int c);
-int	ft_isdigit(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 
-	// pointer to pointer
-
-void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_bzero(void *b, size_t len);
-
-
-	// int n, int fd --> void
+	// write in the file descriptor
 
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
@@ -39,17 +47,3 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_putchar_fd(int n, int fd);
 
 
-	// pointer on function
-
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-
-
-	// char, int --> pointer on char
-
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-
-
-	// 2 strings --> int
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
