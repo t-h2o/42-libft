c83f55a89d8ff8d8f8e24cd17f44b456e22faced
theo <grivel.theo@protonmail.com>
2022-07-28 19:22:25 +0200

    Created header bonus

--- headers/libft.h
+++ headers/libft.h
@@ -22,6 +22,14 @@
 # define FT_64B_MIN		-9223372036854775808
 # define FT_U_64B_MAX	18446744073709551615
 
+	// Type define
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 	// whithout extern functions
 
 int		ft_isalpha(int c);
@@ -67,5 +75,13 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putchar_fd(int n, int fd);
+
+	// BONUS
+
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
+void	ft_lstadd_front(t_list **alst, t_list *new);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **alst, t_list *new);
 
 #endif
